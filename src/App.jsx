014c02f0b0d55b7454a777055115020510720bfa
giovanni-arduini <gionnibigudd@gmail.com>
2025-03-18 14:48:34 +0100

    ADD icon display snack 3

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,25 @@ import { useEffect } from "react";
 import "./App.css";
 import useSwitch from "./useSwitch";
 import useDate from "./useDate";
+import useCustomPointer from "./useCustomPointer";
 
 function App() {
   const [isOn, toggle] = useSwitch();
   const currentDate = useDate();
+  const customPointer = useCustomPointer("🔥");
 
   console.log(isOn);
 
   return (
     <>
       <div>
+        <h1>Sposta il mouse per vedere il cursore personalizzato!</h1>
+        {customPointer}
+      </div>
+      <div>
         <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
         <button onClick={toggle}>Cambia stato</button>
       </div>
-
       <div>
         <h1>Data e ora attuali:</h1>
         <p>{currentDate.toLocaleString()}</p>
---
+++ src/useCustomPointer.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+function useCustomPointer(newPointer) {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      setMousePosition({ x: e.clientX, y: e.clientY });
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  return <div>{newPointer}</div>;
+}
+
+export default useCustomPointer;
